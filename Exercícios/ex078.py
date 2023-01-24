29fb68da40ab4738d0982c4cd7f1f8fd4fb9a740
PedroHDev <85499142+PedroJunior1630@users.noreply.github.com>
2023-01-24 18:28:26 -0300

    Update ex078.py

--- Exercícios/ex078.py
+++ Exercícios/ex078.py
@@ -1,4 +1,3 @@
-#Passo 1 ler 5 valores e guardar em uma lista
 valores = [int(input('Digite o valor para a posição 0: ')),
            int(input('Digite o valor para a posição 1: ')),
            int(input('Digite o valor para a posição 2: ')),
@@ -16,5 +15,3 @@ print(f'\nMenor valor: {mi} nas posições: ',end = "")
 for pos,valor in enumerate(valores):
     if valor == mi:
         print(f'\033[1;33m{pos}...',end = " ")
-
-#Passo 3 mostrar as respectivas posições dos números
