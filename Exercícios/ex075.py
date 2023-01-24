08c0f85f0cc1069c9b0db10187bd221ded13955b
PedroHDev <85499142+PedroJunior1630@users.noreply.github.com>
2023-01-24 18:29:53 -0300

    Update ex075.py

--- Exercícios/ex075.py
+++ Exercícios/ex075.py
@@ -5,11 +5,12 @@ num4 = int(input('Digite o 4° número: '))
 
 numeros = (num1,num2,num3,num4)
 print(f'O número 9 apareceu: {numeros.count(9)} vezes')
+
 if 3 in numeros:
     print(f'O número 3 apareceu  pela primeira vez na: {numeros.index(3) + 1}° posição')
 else:
     print('O número 3 não foi digitado')
-c = 0
+
 print('Numeros pares: ',end = "")
 for n in numeros:
     if n % 2 == 0:
