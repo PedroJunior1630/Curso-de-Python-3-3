601ab2242b7bb9d55b23a4ee35d067bf2353c28b
pedrojunior <ph751153@gmail.com>
2023-12-29 14:16:22 -0300

    EX094 Alterado

--- Exercícios/ex094.py
+++ Exercícios/ex094.py
@@ -1,7 +1,7 @@
-import copy
 lista = []
 dic = {}
 cont = soma = 0
+
 while True:
     cont += 1
     print("=+=" * 20)
@@ -9,11 +9,12 @@ while True:
     print("=+=" * 20)
     dic["nome"] = str(input('Nome: ')).strip().title()
     dic["idade"] = int(input('Idade: '))
+    soma  += dic["idade"]
     dic["sexo"] = str(input('Sexo[F/M]: ')).upper().strip()
     while dic["sexo"] != "M" and dic["sexo"] != "F":
         print("ERRO! Os valores aceitos são apenas F e M")
         dic["sexo"] = str(input('Sexo[F/M]: ')).upper().strip()
-    lista.append(copy.deepcopy(dic))
+    lista.append(dic.copy())
     print("=+=" * 10)
     opc = str(input('Deseja continuar?[S/N]: ')).strip().upper()
     while opc != "S" and opc != "N":
@@ -23,28 +24,19 @@ while True:
         break
 
 print("=+=" * 20)
-print(f"(A)Tivemos {cont} pessoas cadastradas.")
+print(f"(A) Tivemos {len(lista)} pessoas cadastradas.")
 
-for contador,nada in enumerate(lista):
-    for chave,valor in lista[contador].items():
-        if chave == "idade":
-            soma += valor
-media = soma / cont
+media = soma / len(lista)
 print(f"(B) A média de idade deste grupo é de {media:.1f} anos.")
 
-
 print("(C) As mulheres do grupo são: ",end = "")
-for contador,nada in enumerate(lista):
-    for chave,valor in lista[contador].items():
-        if chave == "sexo" and valor == "F":
-            for c,v in lista[contador].items():
-                if c == "nome":
-                    print(f'{v};',end = " ")
+for mulheres in lista:
+    if mulheres["sexo"] == "F":
+        print(f'{mulheres["nome"]};',end = " ")
 
 print('\n(D) Lista de pessoas acima da média: ')
-for contador,nada in enumerate(lista):
-    for chave, valor in lista[contador].items():
-        if chave == "idade" and valor > media:
-            for c,v in lista[contador].items():
-                print(f'{c} = {v};',end = " ")
-            print()
+for pessoas in lista:
+    if pessoas["idade"] > media:
+        for chave, valor in pessoas.items():
+            print(f'{chave} = {valor};',end = " ")
+        print()
