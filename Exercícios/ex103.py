d08551e0fed7d2abbda59a7b0ab4330161309a78
pedrojunior <ph751153@gmail.com>
2024-01-03 18:17:47 -0300

    EX103 Finalizado e alterado

--- Exercícios/ex103.py
+++ Exercícios/ex103.py
@@ -1,17 +1,17 @@
-def ficha(jogador="indefinido",gols="0"):
+def ficha(jogador="desconhecido",gols="0"):
     print("+=+" *10)
-    print(f"O jogador {jogador} marcou {int(gols)} gols.")
+    print(f"O jogador {jogador} marcou {int(gols)} gol(s).")
 
 
 print("+=+" * 10)
 jog = str(input("Nome do jogador: ")).strip()
-gol = str(input("Marcou quantos gols: ")).strip()
+gol = input("Marcou quantos gols: ")
 
-if jog == "" and gol == "":
+if jog == "" and gol.isnumeric() == False:
     ficha()
 elif jog == "":
     ficha(gols=gol)
-elif gol == "":
+elif gol.isnumeric() == False:
     ficha(jog)
 else:
     ficha(jog,gol)
