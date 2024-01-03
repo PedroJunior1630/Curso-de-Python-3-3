070cbd8d0f2f6a921c994737386a16b677927694
pedrojunior <ph751153@gmail.com>
2024-01-03 15:29:45 -0300

    EX104 Finalziado

---
+++ Exercícios/ex104.py
@@ -0,0 +1,11 @@
+def leiaInt(msgInput):
+    while True:
+        num = input(f"\033[1;97m{msgInput}")
+        if num.isnumeric() == True:
+            break
+        else:
+            print(f"\033[1;31mERRO o valor digitado não é um número!")
+    return num
+
+n = leiaInt("Digite um número: ")
+print(f"Você digitou o número {n}")
