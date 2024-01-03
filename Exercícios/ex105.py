b6f99931069fb38812359b920be6f0ec575d9b26
pedrojunior <ph751153@gmail.com>
2024-01-03 18:27:58 -0300

    EX105 Finalizado e alterado

--- Exercícios/ex105.py
+++ Exercícios/ex105.py
@@ -1,5 +1,3 @@
-dicionario = {}
-
 def notas(*notas,sit=False):
     """
     -> Função para analisar a média de uma turma pela leitura de várias notas
@@ -7,12 +5,13 @@ def notas(*notas,sit=False):
     sit: situação a ser mostrada caso seja True, False é valor padrão.
     return: retorna um dicionário com várias informações
     """
+    dicionario = {}
     print("+=+" * 20)
     dicionario["Quantidade"] = len(notas) 
     dicionario["Maior nota"] = max(notas)
     dicionario["Menor nota"] = min(notas)
     dicionario["Média da turma"] = sum(notas) / len(notas)
-    if sit == True:
+    if sit:
         if dicionario["Média da turma"] <= 5:
             dicionario["Situação"] = "RUIM"
         elif 5 < dicionario["Média da turma"] <= 7:
@@ -22,5 +21,5 @@ def notas(*notas,sit=False):
     return dicionario
 
 
-resp = notas(9.7,7.6,5.4,6,5.4,5,sit=True)
+resp = notas(9.7,7.6,5.4,6,5.4,5,8.8,10,1.4,4,3)
 print(resp)
