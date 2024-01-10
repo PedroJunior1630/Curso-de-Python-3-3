8d61d1bf6a848e141ff6bb2aa4d426970a241b3c
pedrojunior <ph751153@gmail.com>
2024-01-10 18:58:56 -0300

    EX115 interface de interação pronta

--- Exercícios/ex115.py
+++ Exercícios/ex115.py
@@ -1,4 +1,15 @@
 from ex115.lib.interface import *
-
-cabecalho("MENU PRINCIPAL")
-menu(["Visualizar a lista de Pessoas","Cadastrar Pessoas","Sair do Programa"])
+from time import sleep
+while True:
+    menu(["Visualizar a lista de Pessoas","Cadastrar Pessoas","Sair do Programa"])
+    opcao = leiaInt("\033[32mQual opção deseja?\033[m")
+    if opcao == 1:
+        cabecalho("OPÇÃO 1")
+    elif opcao == 2:
+        cabecalho("OPÇÃO 2")
+    elif opcao == 3:
+        cabecalho("Sistema finalizando... Até logo!")
+        break
+    else:
+        print("\033[31mERRO! Digite uma opção válida!")
+    sleep(2)
--- Exercícios/ex115/lib/interface/__init__.py
+++ Exercícios/ex115/lib/interface/__init__.py
@@ -1,11 +1,27 @@
+def leiaInt(msg):
+    while True:
+        try:
+            num = int(input(f"\033[1;97m{msg}"))
+        except KeyboardInterrupt:
+            print("\033[1;31m O usuario preferiu não digitar o número.")
+        except:
+            print(f'\033[1;31mERRO! Por favor digite um número inteiro válido.')
+        else:
+            return num
+
+
 def linha(tam=15):
     print("=+=" * tam)
+
 
 def cabecalho(texto):
     linha()
     print(texto.center(90))
     linha()
 
+
 def menu(opc):
+    cabecalho("MENU PRINCIPAL")
     for contador, item in enumerate(opc):
         print(f'\033[36m{contador+1}-\033[m \033[97m{item}\033[m')
+
