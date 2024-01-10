ba3bc3d638ed97d4f33470570d44a2f68013d3ce
pedrojunior <ph751153@gmail.com>
2024-01-10 20:22:13 -0300

    EX115 finalizado

--- Exercícios/ex115.py
+++ Exercícios/ex115.py
@@ -8,10 +8,13 @@ while True:
     menu(["Visualizar a lista de Pessoas","Cadastrar Pessoas","Sair do Programa"])
     opcao = leiaInt("\033[32mQual opção deseja?\033[m")
     if opcao == 1:
-        cabecalho("OPÇÃO 1")
+        cabecalho("PESSOAS CADASTRADAS")
         exibirArquivo(arquivo)
     elif opcao == 2:
-        cabecalho("OPÇÃO 2")
+        cabecalho("NOVO CADASTRO")
+        nome = str(input("Nome: ")).strip().title()
+        idade = leiaInt("Idade: ")
+        cadastrarPessoa(arquivo,nome,idade)
     elif opcao == 3:
         cabecalho("Sistema finalizando... Até logo!")
         break
--- Exercícios/ex115/lib/arquivo/__init__.py
+++ Exercícios/ex115/lib/arquivo/__init__.py
@@ -3,10 +3,8 @@ def arquivoExiste(arq):
     try:
         var = open(arq,"rt")
     except:
-        print("ERROR")
         return False
     else:
-        print("EXISTE")
         return True
 
 
@@ -21,6 +19,22 @@ def exibirArquivo(arq):
     except:
         print("ERRO AO EXIBIR O ARQUIVO")
     else:
-        cabecalho("PESSOAS CADASTRADAS")
-        print(var.read())
+        for linha in var:
+            dado = linha.split(";")
+            dado[1] = dado[1].replace("\n","")
+            print(f"{dado[0]:.<30}{dado[1]:>3} anos")
+    finally:
+        var.close()
 
+def cadastrarPessoa(arq, n, i):
+    try:
+        var = open(arq, "at")
+    except:
+        print("Houve um ERRO na abertura do arquivo.")
+    else:
+        try:
+            var.write(f"{n};{i}\n")
+        except:
+            print("Houve um ERRO no ao registrar os dados.")
+        else:
+            print(f"Novo registro de {n} adicionado.")
